08a6197e25164880ec9ff903300c3906010db2b3
James <gacutanjms@gmail.com>
2022-10-10 10:29:48 +0800

    Added functionality for calculator

--- main.js
+++ main.js
@@ -35,39 +35,132 @@ function operate(a, operation, b) {
 }
 
 
-// store variable when a numb key is click
+// store value in num when a numb key is click
 
 
 let num = [];
+let leftNum = [];
+let operator;
+let rightNum = [];
+
+const left = document.querySelector('.display-num');
+const right = document.querySelector('.right-num')
+const keys = document.querySelectorAll('.num');
+const keyArray = Array.from(keys);
 
 
-function onClick(key) {
+function onDigitClick(key) {
     key.addEventListener('click', (e) => {
-        if (num.length <= 15) {
+        // reset
+        operatorText.textContent = '';
+        answer.textContent = '0';
+
+        if (num.length <= 14) {
             num += key.textContent;
         } 
 
         if (num.length == 11) {
-            calculate.style.fontSize = '1.4rem';
+            left.style.fontSize = '1.4rem';
+            // right.style.fontSize = '1.4rem';
         }
 
-
         // display array per numb click
-        calculate.textContent = num;
+        left.textContent = num;
+
         return num;
     });
 }
-
-
-
-
-const calculate = document.querySelector('.calculate');
-const keys = document.querySelectorAll('.num');
-const keyArray = Array.from(keys);
-
 keyArray.forEach(key => {
-        onClick(key);
+    onDigitClick(key);
 });
+
+
+// assign num current value to a num variable and empty it
+
+function splitter() {
+
+    if (leftNum.length == 0) {
+        leftNum = Number(num);
+        num = [];
+        left.textContent = '';
+    } else if (num == '') {
+        console.log('pls click a key')
+    } else {
+        rightNum = Number(num);
+        operatorText.textContent = '';
+    }
+}
+
+// make the operator clickable
+
+const addOperator = document.querySelector('.addition');
+const subtractOperator = document.querySelector('.subtraction');
+const multiplyOperator = document.querySelector('.multiplication');
+const divideOperator = document.querySelector('.division');
+
+let operatorText = document.querySelector('.operation');
+
+
+function onOperationClick(operation) {
+    operation.addEventListener('click', () => {
+        operator = operation.textContent;
+
+        splitter();
+
+        // display operation in screen
+        operatorText.textContent = operator;
+    });
+}
+
+onOperationClick(addOperator);
+onOperationClick(subtractOperator);
+onOperationClick(multiplyOperator);
+onOperationClick(divideOperator);
+
+
+// calculate when equal is clicked 
+
+const equals = document.querySelector('.equals');
+const answer = document.querySelector('.answer');
+
+equals.addEventListener('click', () => {
+    splitter();
+
+    // display answer in screen
+    answer.textContent = operate(leftNum, operator, rightNum);
+
+    // reset
+    left.textContent = ''; 
+    num = [];
+    leftNum = [];
+    rightNum = [];
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
